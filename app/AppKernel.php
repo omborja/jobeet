65fd9a5297ce3d958f708ecd62e8f745d6f50175
omborja <borjaomr@gmail.com>
2018-04-13 16:24:37 +0100

    security_backend_part

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,12 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Ens\JobeetBundle\EnsJobeetBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Sonata\CacheBundle\SonataCacheBundle(),
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
--- src/Ens/JobeetBundle/Controller/DefaultController.php
+++ src/Ens/JobeetBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace Ens\JobeetBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Security\Core\Security;
 
 
 class DefaultController extends Controller
@@ -14,5 +15,48 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('EnsJobeetBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction()
+    {
+        $request = $this->get('request');
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(Security::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(Security::AUTHENTICATION_ERROR);
+        } else {
+            $error = $session->get(Security::AUTHENTICATION_ERROR);
+            $session->remove(Security::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render('EnsJobeetBundle:Default:login.html.twig', array(
+            // last username entered by the user
+            'last_username' => $session->get(Security::LAST_USERNAME),
+            'error'         => $error,
+        ));
+    }
+
+    /**
+     * @Route("/login_check", name="login_check")
+     */
+    public function loginCheckAction()
+    {
+        // this controller will not be executed,
+        // as the route is handled by the Security system
+        throw new \Exception('Which means that this Exception will not be raised anytime soon …');
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutAction()
+    {
+        // this controller will not be executed,
+        // as the route is handled by the Security system
+        throw new \Exception('Which means that this Exception will not be raised anytime soon …');
     }
 }
